ddc0104528ff7a3a91f1f0745dc05e525858c84d
Michał Frąckiewicz <hello@systemz.pl>
2021-01-06 07:52:58 +0100

    Hardcoded username for Docker installation

--- provision/bash/docker.sh
+++ provision/bash/docker.sh
@@ -2,4 +2,4 @@ echo "Installing docker..."
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo bash get-docker.sh
 sudo rm get-docker.sh
-sudo usermod -aG docker $USER
+sudo usermod -aG docker vagrant
