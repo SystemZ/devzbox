2f13d83dca1c9b7c7adbf42c9d7c0484c0ecb43c
Michał Frąckiewicz <hello@systemz.pl>
2021-01-06 09:20:58 +0100

    Create desktop sessions for NX automatically

--- provision/bash/nomachine-7.sh
+++ provision/bash/nomachine-7.sh
@@ -5,3 +5,11 @@ NX_FILE="nomachine_7.0.211_4_amd64.deb"
 wget "https://download.nomachine.com/download/7.0/Linux/$NX_FILE"
 sudo dpkg -i $NX_FILE
 rm $NX_FILE
+
+echo "Configuring NoMachine server"
+# https://forums.nomachine.com/topic/connect-to-physical-display
+sed -i 's/#CreateDisplay 0/CreateDisplay 1/g' /usr/NX/etc/server.cfg
+sed -i 's/#DisplayOwner ""/DisplayOwner "vagrant"/g' /usr/NX/etc/server.cfg
+
+echo "Restarting NoMachine server"
+sudo /etc/NX/nxserver --restart
