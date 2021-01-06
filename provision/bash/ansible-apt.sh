51b1cfcab1dd18dd4966c00809ca4d29080779d1
Michał Frąckiewicz <hello@systemz.pl>
2021-01-06 07:52:19 +0100

    Add script to install Ansible from pip
    Official apt repo for Ubuntu 20.04 doesn't work yet

--- provision/bash/ansible-apt.sh
+++ provision/bash/ansible-apt.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# this still doesn't work for Ubuntu 20.04 @ 2020.01.06
+# https://github.com/ansible/ansible/issues/68645
 apt-get install -y software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt-get install -y ansible
---
+++ provision/bash/ansible-pip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt-get install -y python3-pip
+pip3 install ansible
+#python -m pip install ansible
