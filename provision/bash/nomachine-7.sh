f920419ba1c687d3c957ff039e07ac7773dbed76
Michał Frąckiewicz <SystemZ@users.noreply.github.com>
2021-11-22 00:23:00 +0100

    Update nx download URL

--- provision/bash/nomachine-7.sh
+++ provision/bash/nomachine-7.sh
@@ -1,8 +1,8 @@
 echo "Installing NoMachine remote desktop"
 
 cd /tmp/
-NX_FILE="nomachine_7.0.211_4_amd64.deb"
-wget "https://download.nomachine.com/download/7.0/Linux/$NX_FILE"
+NX_FILE="nomachine_7.7.4_1_amd64.deb"
+wget "https://download.nomachine.com/download/7.7/Linux/$NX_FILE"
 sudo dpkg -i $NX_FILE
 rm $NX_FILE
 
